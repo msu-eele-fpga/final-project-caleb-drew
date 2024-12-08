6e22b7dbb8fd78789f5f03900457cd3d1f642923
Drew Currie <drew.currie@student.montana.edu>
2024-12-07 19:45:47 -0700

    Added additional control logic enable to use for a busy delay

--- hdl/LCD-Controller/clockDivider.vhd
+++ hdl/LCD-Controller/clockDivider.vhd
@@ -7,10 +7,11 @@ entity clock_divider is
     LENGTH_OF_CLOCK_PULSE_PER_MS : integer := 15
   );
   port (
-    count : in unsigned(C_PERIOD - 1 downto 0);
-    clk   : in std_ulogic;
-    rst   : in std_logic;
-    done  : out std_logic
+    count  : in unsigned(C_PERIOD - 1 downto 0);
+    clk    : in std_ulogic;
+    reset  : in std_logic;
+    enable : in std_logic;
+    ready  : out std_logic
   );
 
 end entity clock_divider;
@@ -20,25 +21,31 @@ architecture counter_arch of clock_divider is
   signal counter : unsigned(C_PERIOD + LENGTH_OF_CLOCK_PULSE_PER_MS downto 0);
 
 begin
-  proc_clock_divider : process (clk, rst)
+  proc_clock_divider : process (clk, reset, enable)
   begin
-    if (rst = '0') then
-      if rising_edge(clk) then
-        -- If counter has exceeded or is equal to the COUNTER_LIMIT
-        -- stop counting and assert done and reset counter
-        -- If enable is still true this will continue to count
-        if counter >= count then
-          done    <= '1';
-          counter <= (others => '0');
-        else
-          -- If still counting assert done as false
-          counter <= counter + 1;
-          done    <= '0';
+    if (reset = '0') then
+      if (enable = '1') then
+        if rising_edge(clk) then
+          -- If counter has exceeded or is equal to the COUNTER_LIMIT
+          -- stop counting and assert ready and reset counter
+          -- If enable is still true this will continue to count
+          if counter >= count then
+            ready   <= '1';
+            counter <= (others => '0');
+          else
+            -- If still counting assert ready as false
+            counter <= counter + 1;
+            ready   <= '0';
+          end if;
         end if;
+      else
+        --Not counting, not enabled
+        ready   <= '1';
+        counter <= (others => '0');
       end if;
     else
       counter <= (others => '0');
-      done    <= '0';
+      ready   <= '0';
     end if;
   end process;
 end architecture counter_arch;
