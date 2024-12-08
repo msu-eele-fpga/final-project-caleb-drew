935f27945f64fbbfccc969ead3550eef36f5dc1d
Drew Currie <drew.currie@student.montana.edu>
2024-12-07 20:10:27 -0700

    basic test bench

--- hdl/LCD-Controller/lcd_controller_tb.vhd
+++ hdl/LCD-Controller/lcd_controller_tb.vhd
@@ -29,5 +29,44 @@ architecture testbench of lcd_tb is
       busy_flag       : out std_logic
     );
   end component;
+
+  --Basic signals 
+  signal reset_tb     : std_logic := '1';
+  signal clk_tb       : std_logic := '0';
+  constant CLK_PERIOD : time      := 20 ns;
+
+  --Interface signals
+  --Inputs
+  signal instruction : std_logic_vector(7 downto 0);
+  signal RS          : std_logic;
+  --Outputs
+  signal write_register : std_logic_vector(7 downto 0);
+  signal rs_output      : std_logic;
+  signal ready          : std_logic;
+
 begin
-end architecture;
+  dut_lcd_controller : component lcd_controller
+    generic map(
+      CLK_PERIOD => 20 ns
+    )
+    port map(
+      clk                        => clk_tb,
+      reset                      => reset_tb,
+      write_register(7 downto 0) => instruction,
+      write_register(8)          => rs,
+      rs                         => rs_output,
+      busy_flag                  => ready
+    );
+    --Create clock
+    clk_tb <= not clk_tb after CLK_PERIOD/2;
+
+    stimuli_and_checker : process is
+    begin
+      print("Testing basic linking of files");
+      wait_for_clock_edge(clk_tb);
+      reset_tb <= '0';
+      wait_for_clock_edges(clk_tb, 4);
+      reset_tb <= '1';
+      std.env.finish;
+    end process;
+  end architecture;
