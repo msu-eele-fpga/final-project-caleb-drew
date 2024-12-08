47772a1e314f20a273c07d3836a2421eb0158895
Caleb4884 <147229305+Caleb4884@users.noreply.github.com>
2024-12-08 14:08:49 -0700

    Fixed read isse from output signal

--- hdl/encoder_counter.vhd
+++ hdl/encoder_counter.vhd
@@ -12,52 +12,54 @@ entity encoder_counter is
 end entity;
 
 architecture encoder_counter_arch of encoder_counter is
-  signal previous_input : std_logic_vector(1 downto 0) := "00"; -- "AB" input wiring
+  signal previous_input  : std_logic_vector(1 downto 0) := "00"; -- "AB" input wiring
+  signal internal_counts : integer                      := 0;
 
 begin
 
   Counting : process (clk, rst, input)
   begin
     if (rst = '1') then
-      counts <= 0;
+      internal_counts <= 0;
     elsif (rising_edge(clk)) then
       case previous_input is
         when "00" =>
           if (input = "10") then
-            counts <= counts + 1;
+            internal_counts <= internal_counts + 1;
           elsif (input = "01") then
-            counts <= counts - 1;
+            internal_counts <= internal_counts - 1;
           else
-            counts <= counts;
+            internal_counts <= internal_counts;
           end if;
         when "10" =>
           if (input = "11") then
-            counts <= counts + 1;
+            internal_counts <= internal_counts + 1;
           elsif (input = "00") then
-            counts <= counts - 1;
+            internal_counts <= internal_counts - 1;
           else
-            counts <= counts;
+            internal_counts <= internal_counts;
           end if;
         when "11" =>
           if (input = "01") then
-            counts <= counts + 1;
+            internal_counts <= internal_counts + 1;
           elsif (input = "10") then
-            counts <= counts - 1;
+            internal_counts <= internal_counts - 1;
           else
-            counts <= counts;
+            internal_counts <= internal_counts;
           end if;
         when "01" =>
           if (input = "00") then
-            counts <= counts + 1;
+            internal_counts <= internal_counts + 1;
           elsif (input = "11") then
-            counts <= counts - 1;
+            internal_counts <= internal_counts - 1;
           else
-            counts <= counts;
+            internal_counts <= internal_counts;
           end if;
         when others =>
-          counts <= counts;
+          internal_counts <= internal_counts;
       end case;
       previous_input <= input;
     end if;
+    counts <= internal_counts;
   end process;
 end architecture;
