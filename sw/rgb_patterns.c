3914cc7c706c9a09328b5fff433aa40fb0d2af8a
Caleb4884 <147229305+Caleb4884@users.noreply.github.com>
2024-12-09 20:30:58 -0700

    Created adc device treee

---
+++ sw/rgb_patterns.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+// TODO: update these offsets if your address are different
+#define GLOBAL_PERIOD_OFFSET 0x0
+#define RED_DUTY_CYCLE_OFFSET 0x4
+#define GREEN_DUTY_CYCLE_OFFSET 0x08
+#define BLUE_DUTY_CYCLE_OFFSET 0X0C
+
+int main()
+{
+    FILE *file;
+    size_t ret;
+    uint32_t val;
+
+    file = fopen("/dev/rgb_controller", "rb+");
+    if (file == NULL)
+    {
+        printf("failed to open file\n");
+        exit(1);
+    }
+
+    // Test reading the registers sequentially
+    printf("\n************************************\n*");
+    printf("* read initial register values\n");
+    printf("************************************\n\n");
+
+    ret = fread(&val, 4, 1, file);
+    printf("HPS_LED_control = 0x%x\n", val);
+
+    ret = fread(&val, 4, 1, file);
+    printf("base period = 0x%x\n", val);
+
+    ret = fread(&val, 4, 1, file);
+    printf("LED_reg = 0x%x\n", val);
+
+    // Reset file position to 0
+    ret = fseek(file, 0, SEEK_SET);
+    printf("fseek ret = %d\n", ret);
+    printf("errno =%s\n", strerror(errno));
+
+    printf("\n************************************\n*");
+    printf("* write values\n");
+    printf("************************************\n\n");
+    // Turn on software-control mode
+    val = 0x01;
+    ret = fseek(file, GLOBAL_PERIOD_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    // We need to "flush" so the OS finishes writing to the file before our code continues.
+    fflush(file);
+
+    // Write some values to the LEDs
+    printf("writing patterns to LEDs....\n");
+    val = 0x55;
+    ret = fseek(file, GREEN_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    sleep(1);
+
+    val = 0xaa;
+    ret = fseek(file, GREEN_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    sleep(1);
+
+    val = 0xff;
+    ret = fseek(file, GREEN_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    usleep(0.5e6);
+
+    val = 0x00;
+    ret = fseek(file, GREEN_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    sleep(1);
+
+    // Turn on hardware-control mode
+    printf("back to hardware-control mode....\n");
+    val = 0x00;
+    ret = fseek(file, GLOBAL_PERIOD_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    val = 0x12;
+    ret = fseek(file, RED_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    sleep(5);
+
+    // Speed up the base period!
+    val = 0x02;
+    ret = fseek(file, RED_DUTY_CYCLE_OFFSET, SEEK_SET);
+    ret = fwrite(&val, 4, 1, file);
+    fflush(file);
+
+    printf("\n************************************\n*");
+    printf("* read new register values\n");
+    printf("************************************\n\n");
+
+    // Reset file position to 0
+    ret = fseek(file, 0, SEEK_SET);
+
+    ret = fread(&val, 4, 1, file);
+    printf("HPS_LED_control = 0x%x\n", val);
+
+    ret = fread(&val, 4, 1, file);
+    printf("base period = 0x%x\n", val);
+
+    ret = fread(&val, 4, 1, file);
+    printf("LED_reg = 0x%x\n", val);
+
+    fclose(file);
+    return 0;
+}
